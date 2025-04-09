33ad030e2d3be43eb7385e7da6e51ada14cd4177
Hussein Anwer <hanwer@uccs.edu>
2025-04-08 22:30:24 -0600

    cond test method set up, we're comparing two different inputs and if the first input is similair to the second we write hello,if not then  bye

--- script.sh
+++ script.sh
@@ -2,5 +2,11 @@
 #!/usr/bin/env bash
 #Hussein Anwer
 #Date:2025/4/8
-
-
+#Copyright(c) Hussein Anwer
+condTest(){
+    if [[ $1 == $2 ]]; then
+        echo "hello"
+    else
+        echo "bye"
+    fi
+}
