701a5b22992b42460f3aeaf38785c4317a864387
Hussein Anwer <hanwer@uccs.edu>
2025-04-08 22:52:39 -0600

    created the sum number check method to see if the user understand the addition of the 2 given numbers and return correct or wrong if the user is correct or not

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#!/usr/bin/env bash
 #Hussein Anwer
 #Date:2025/4/8
 #Copyright(c) Hussein Anwer
@@ -7,6 +6,18 @@ condTest(){
     if [[ $1 == $2 ]]; then
         echo "hello"
     else
-        echo "bye"
+        echo "bye "
+    fi
+}
+sumNumCheck(){
+    num1=5
+    num2=7
+    echo "what is the sum of $num1 and $num2 equal to?"
+    sum=$((num1 + num2))
+    read userInput
+    if [[ $userInput -eq $sum ]]; then
+        echo "correct"
+    else
+        echo "incorrect"
     fi
 }
